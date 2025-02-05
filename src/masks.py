9836f22c2e0d134e0bc8a94bf5e54d91b295fa7c
Aleksey Zvyagintsev <alex0236889@yandex.ru>
2025-02-05 17:31:39 +0300

    first add SRC course projec

---
+++ src/masks.py
@@ -0,0 +1,34 @@
+###########################################################################
+
+from typing import Optional
+
+
+def get_mask_card_number(card_number: Optional[int] = None) -> str:
+    """
+    Функция get_mask_card_number принимает на вход номер карты в виде 16-ти значное числа
+    и выводит замаскированный вариант в формате XXXX XX** **** XXXX
+    """
+    card_number_str = str(card_number)
+    if len(card_number_str) > 16 or len(card_number_str) < 16:
+        return "Введите 16-ти значное число"
+    elif card_number is None:
+        return "Введите 16-ти значное число"
+    else:
+        return f"{card_number_str[0:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
+
+
+def get_mask_account(account_number: Optional[int] = None) -> str:
+    """
+    Функция принимает на вход 20-ти значное число (номер счета)
+    и возвращает маску номера по правилу **XXXX
+    """
+    account_number_str = str(account_number)
+    if len(account_number_str) > 20 or len(account_number_str) < 20:
+        return "Введите 20-ти значное число"
+    elif account_number is None:
+        return "Введите 20-ти значное число"
+    else:
+        return f"**{account_number_str[-4:]}"
+
+
+#####################################################################
