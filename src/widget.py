1dc99467824eaa5aee4a72798fd576cf794392d3
Aleksey Zvyagintsev <alex0236889@yandex.ru>
2025-02-06 15:58:36 +0300

    fixing arguments None in widget

--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,16 @@
-from typing import Optional, Union
+from typing import Optional
 
 from masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(account_card: Union[str, int]) -> str:
+def mask_account_card(account_card: Optional[str] = None) -> str:
     """
     Принимать один аргумент — строку, содержащую тип и номер карты или счета.
     Возвращать строку с замаскированным номером.
     """
-    if "Счет" in account_card:
+    if account_card is None:
+        return "Принимать один аргумент — строку, содержащую тип и номер карты или счета."
+    elif "Счет" in account_card:
         result = get_mask_account(int(account_card[-20:]))
         return f"{account_card[0:-20]}{result}"
     else:
@@ -23,6 +25,9 @@ def get_date(date_and_time: Optional[str] = None) -> str:
     и возвращает строку с датой в формате
     "ДД.ММ.ГГГГ"
     """
-    split_date_and_time = date_and_time.split("T")
-    split_date = split_date_and_time[0].split("-")
-    return f"{split_date[2]}.{split_date[1]}.{split_date[0]}"
+    if date_and_time is None:
+        return "принимает на вход строку с датой в формате '2024-03-11T02:26:18.671407'"
+    else:
+        split_date_and_time = date_and_time.split("T")
+        split_date = split_date_and_time[0].split("-")
+        return f"{split_date[2]}.{split_date[1]}.{split_date[0]}"
