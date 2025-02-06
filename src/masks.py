5bc6533410d74bb6332a50f3cfadc43a4d8bfeb3
Aleksey Zvyagintsev <alex0236889@yandex.ru>
2025-02-06 16:01:49 +0300

    fixing arguments None in masks

--- src/masks.py
+++ src/masks.py
@@ -9,7 +9,9 @@ def get_mask_card_number(card_number: Optional[int] = None) -> str:
     и выводит замаскированный вариант в формате XXXX XX** **** XXXX
     """
     card_number_str = str(card_number)
-    if len(card_number_str) > 16 or len(card_number_str) < 16:
+    if card_number is None:
+        return "Функция get_mask_card_number принимает на вход номер карты в виде 16-ти значное числа"
+    elif len(card_number_str) > 16 or len(card_number_str) < 16:
         return "Введите 16-ти значное число"
     elif card_number is None:
         return "Введите 16-ти значное число"
@@ -23,6 +25,8 @@ def get_mask_account(account_number: Optional[int] = None) -> str:
     и возвращает маску номера по правилу **XXXX
     """
     account_number_str = str(account_number)
+    if account_number is None:
+        return "Функция принимает на вход 20-ти значное число (номер счета)"
     if len(account_number_str) > 20 or len(account_number_str) < 20:
         return "Введите 20-ти значное число"
     elif account_number is None:
