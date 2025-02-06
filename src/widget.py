97fbe25f568f35858ddfee5c5b709c18be5bd1ae
Aleksey Zvyagintsev <alex0236889@yandex.ru>
2025-02-06 13:56:43 +0300

    add def get_date() and docstrings in widget

--- src/widget.py
+++ src/widget.py
@@ -1,9 +1,13 @@
-from typing import Union
+from typing import Union, Optional
 
 from masks import get_mask_card_number
 from masks import get_mask_account
 
 def mask_account_card(account_card: Union[str, int]) -> str:
+    """
+    Принимать один аргумент — строку, содержащую тип и номер карты или счета.
+    Возвращать строку с замаскированным номером.
+    """
     if "Счет" in account_card:
         result = get_mask_account(int(account_card[-20:]))
         return f"{account_card[0:-20]}{result}"
@@ -11,3 +15,14 @@ def mask_account_card(account_card: Union[str, int]) -> str:
         result = get_mask_card_number(int(account_card[-16:]))
         return f"{account_card[0:-16]}{result}"
 
+
+def get_date(date_and_time: Optional[str]=None) -> str:
+    """
+    принимает на вход строку с датой в формате
+    "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате
+    "ДД.ММ.ГГГГ"
+    """
+    split_date_and_time = date_and_time.split("T")
+    split_date = split_date_and_time[0].split("-")
+    return f"{split_date[2]}.{split_date[1]}.{split_date[0]}"
