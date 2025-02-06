3e913e474565dd4909133db1fe6a4efe159ac7a8
Aleksey Zvyagintsev <alex0236889@yandex.ru>
2025-02-06 16:16:03 +0300

    fixing import in widget

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_card: Optional[str] = None) -> str:
