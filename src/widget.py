cfa599fc2a7bfd2a92fbc814245db52093357dd8
Aleksey Zvyagintsev <alex0236889@yandex.ru>
2025-02-06 12:49:58 +0300

    add def mask_account_card() in widget

---
+++ src/widget.py
@@ -0,0 +1,14 @@
+from typing import Union
+
+from masks import get_mask_card_number
+from masks import get_mask_account
+
+def mask_account_card(account_card: Union[str, int]) -> str:
+    if "Счет" in account_card:
+        result = get_mask_account(int(account_card[-20:]))
+        return f"{account_card[0:-20]}{result}"
+    else:
+        result = get_mask_card_number(int(account_card[-16:]))
+        return f"{account_card[0:-16]}{result}"
+
+print(mask_account_card("Maestro 1596837868705199"))
