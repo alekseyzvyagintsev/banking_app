37ef980a9b23adf03093ee42065f4050a7942e9a
Aleksey Zvyagintsev <alex0236889@gmail.com>
2025-02-12 19:21:53 +0300

    Добавлена функция sort_by_date в файл processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,27 @@
 
 def filter_by_state(list_of_dict: list[dict], state='EXECUTED') -> list[dict]:
-    '''
+    """
     принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED').
     Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
     соответствует указанному значению.
-    '''
+    """
     result = list(filter(lambda i: i["state"] == state, list_of_dict))
     return result
 
 
+def sort_by_date(list_of_dict: list[dict], descending=True) -> list[dict]:
+    """
+    принимает список словарей и необязательный параметр,
+    задающий порядок сортировки (по умолчанию — убывание).
+    Функция возвращает новый список, отсортированный по дате (date).
+    """
+    sorted_list = sorted(list_of_dict, key=lambda x: x['date'], reverse=descending)
+    return sorted_list
+
+
+print(sort_by_date([
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]))
