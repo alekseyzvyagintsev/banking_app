e7ec0fe0549be2da5018f40958791a2716465ec3
Aleksey Zvyagintsev <alex0236889@gmail.com>
2025-02-12 21:33:05 +0300

    исправлены аннотации к функциям в файле processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,6 @@
+from typing import Iterable, Optional, Any
 
-def filter_by_state(list_of_dict: list[dict], state='EXECUTED') -> list[dict]:
+def filter_by_state(list_of_dict: Iterable[dict[str, Any]], state: Optional[str]="EXECUTED") -> list[dict[str, Any]]:
     """
     принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED').
     Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
@@ -9,19 +10,11 @@ def filter_by_state(list_of_dict: list[dict], state='EXECUTED') -> list[dict]:
     return result
 
 
-def sort_by_date(list_of_dict: list[dict], descending=True) -> list[dict]:
+def sort_by_date(list_of_dict: Iterable[dict[str, Any]], descending: bool=True) -> list[dict[str, Any]]:
     """
     принимает список словарей и необязательный параметр,
     задающий порядок сортировки (по умолчанию — убывание).
     Функция возвращает новый список, отсортированный по дате (date).
     """
-    sorted_list = sorted(list_of_dict, key=lambda x: x['date'], reverse=descending)
+    sorted_list = sorted(list_of_dict, key=lambda x: x["date"], reverse=descending)
     return sorted_list
-
-
-print(sort_by_date([
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-]))
