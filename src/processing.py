16e1d3d03f794411556ab6bd5325454158c15e02
Aleksey Zvyagintsev <alex0236889@gmail.com>
2025-02-13 22:07:44 +0300

    Изменил имя первого параметра в функциях модуля processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,20 +1,23 @@
 from typing import Iterable, Optional, Any
 
-def filter_by_state(list_of_dict: Iterable[dict[str, Any]], state: Optional[str]="EXECUTED") -> list[dict[str, Any]]:
+
+def filter_by_state(
+    list_of_actions: Iterable[dict[str, Any]], state: Optional[str] = "EXECUTED"
+) -> list[dict[str, Any]]:
     """
     принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED').
     Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
     соответствует указанному значению.
     """
-    result = list(filter(lambda i: i["state"] == state, list_of_dict))
+    result = list(filter(lambda i: i["state"] == state, list_of_actions))
     return result
 
 
-def sort_by_date(list_of_dict: Iterable[dict[str, Any]], descending: bool   =True) -> list[dict[str, Any]]:
+def sort_by_date(list_of_actions: Iterable[dict[str, Any]], descending: bool = True) -> list[dict[str, Any]]:
     """
     принимает список словарей и необязательный параметр,
     задающий порядок сортировки (по умолчанию — убывание).
     Функция возвращает новый список, отсортированный по дате (date).
     """
-    sorted_list = sorted(list_of_dict, key=lambda x: x["date"], reverse=descending)
+    sorted_list = sorted(list_of_actions, key=lambda x: x["date"], reverse=descending)
     return sorted_list
