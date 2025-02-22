d553e5a999e3de2e4700abd34244beff3e39e7c7
Aleksey Zvyagintsev <alex0236889@gmail.com>
2025-02-22 22:02:51 +0300

    Исправлены доработаны тесты и модуль widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,4 @@
+############################################################################
 import datetime
 
 from typing import Optional
@@ -10,23 +11,19 @@ def mask_account_card(account_card: Optional[str] = None) -> str:
     Принимает один аргумент — строку, содержащую тип и номер карты или счета.
     Возвращать строку с замаскированным номером.
     """
-    if account_card is None:
-        return "Ожидается не пустая строка"
-    elif type(account_card) != str:
+    if type(account_card) != str:
         return f"Ожидается строка, это ({account_card}) не строка"
     else:
         split_account_card = list(account_card.split(" "))
         if (split_account_card[-1]) != "":
-            if len(split_account_card[-1]) == 20 and len(split_account_card[-1]) == 16:
-                return "Функция принимает один аргумент — строку, содержащую тип и номер карты или счета."
+            if len(split_account_card[-1]) != 20 and len(split_account_card[-1]) != 16:
+                return f"Номер карты = 16-ти символам, а счета 20-ти. Здесь ({account_card}) не правильно"
             elif len(split_account_card[-1]) == 20:
                 result = get_mask_account(int(split_account_card[-1]))
                 return f"{account_card[0:-20]}{result}"
             elif len(split_account_card[-1]) == 16:
                 result = get_mask_card_number(int(split_account_card[-1]))
                 return f"{account_card[0:-16]}{result}"
-            else:
-                return f"Номер карты = 16-ти символам, а счета 20-ти. Здесь ({account_card}) не правильно"
         else:
             return "Номер карты или счета должен быть без пробелов и других симболов после номера."
 
@@ -42,10 +39,12 @@ def get_date(date_and_time: Optional[str] = None) -> str:
     try:
         datetime.datetime.strptime(date_and_time, "%Y-%m-%dT%H:%M:%S.%f")
     except ValueError:
-        return "Ожидается строка в формате '2024-03-11T02:26:18.671407'. Это {date_and_time} не правильный формат"
+        return "Ожидается строка в формате '2024-03-11T02:26:18.671407'. Введено не верно, попробуйте ещё раз"
     except TypeError:
-        return "Ожидается строка в формате '2024-03-11T02:26:18.671407'. Это {date_and_time} не строка"
+        return "Ожидается строка в формате '2024-03-11T02:26:18.671407'. Введено не верно, попробуйте ещё раз"
     else:
         split_date_and_time = date_and_time.split("T")
         split_date = split_date_and_time[0].split("-")
         return f"{split_date[2]}.{split_date[1]}.{split_date[0]}"
+
+############################################################################
