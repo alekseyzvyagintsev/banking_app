bd4224cc536ffd9e1bd6bba1a50edd2764a8f7f0
Aleksey Zvyagintsev <alex0236889@gmail.com>
2025-03-08 15:15:51 +0300

    Добавлена проверка пустых строк в функциях модуля processing.py

--- src/processing.py
+++ src/processing.py
@@ -10,16 +10,20 @@ def filter_by_state(
     Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
     соответствует указанному значению.
     """
+    if not state:
+        state = "EXECUTED"
     result = list(filter(lambda i: i["state"] == state, list_of_actions))
     return result
 
 
-def sort_by_date(list_of_actions: Iterable[dict[str, Any]], descending: bool = True) -> list[dict[str, Any]]:
+def sort_by_date(list_of_actions: Iterable[dict[str, Any]], descending: Optional[bool] = True) -> list[dict[str, Any]]:
     """
     принимает список словарей и необязательный параметр,
     задающий порядок сортировки (по умолчанию — убывание).
     Функция возвращает новый список, отсортированный по дате (date).
     """
+    if descending is not False:
+        descending = True
     sorted_list = sorted(list_of_actions, key=lambda x: x["date"], reverse=descending)
     return sorted_list
 
