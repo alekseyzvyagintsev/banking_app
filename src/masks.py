77cf183e8aff32add08ea0f3733a157b0cb3f6fb
Aleksey Zvyagintsev <alex0236889@gmail.com>
2025-03-22 23:50:09 +0300

    Add moduls: logging_masks.py logging_utils.py Add ligging in moduls: masks.py utils.py

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,6 @@
 ###########################################################################
 from typing import Optional
+from src.logging_masks import logger
 
 
 def get_mask_card_number(card_number: Optional[int] = None) -> str:
@@ -7,13 +8,38 @@ def get_mask_card_number(card_number: Optional[int] = None) -> str:
     Функция get_mask_card_number принимает на вход номер карты в виде 16-ти значное числа
     и выводит замаскированный вариант в формате XXXX XX** **** XXXX
     """
-    card_number_str = str(card_number)
+    # Регистрация входных данных
+    logger.info(f"Входные данные: card_number={card_number}")
+
+    # Проверка типа аргумента
     if type(card_number) is not int:
+        logger.warning("Тип аргумента не является целым числом.")
         return "Введите 16-ти значное число"
-    elif len(card_number_str) != 16:
-        return "Введите 16-ти значное число"
-    else:
-        return f"{card_number_str[0:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
+
+    # Преобразование аргумента в строку
+    card_number_str = str(card_number)
+
+    # Проверка длины строки
+    if len(card_number_str) != 16:
+        logger.warning("Длина строки не равна 16 символам.")
+        return "!= 16. Введите 16-ти значное число"
+
+    # Форматирование номера карты
+    masked_card_number = f"{card_number_str[0:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
+
+    # Регистрация результата
+    logger.info(f"Маскированный номер карты: {masked_card_number}")
+
+    return masked_card_number
+
+if __name__ == '__main__':
+    # Правильное значение
+    print(get_mask_card_number(1234567890123456))  # Должна вернуть замаскированное значение
+
+    # Неправильные значения
+    print(get_mask_card_number(123))  # Длина меньше 16 символов
+    print(get_mask_card_number(12.34))  # Тип не int
+    print(get_mask_card_number(12345678901234562)) # Длина Bolshe 16 символове
 
 
 def get_mask_account(account_number: Optional[int] = None) -> str:
@@ -21,13 +47,37 @@ def get_mask_account(account_number: Optional[int] = None) -> str:
     Функция принимает на вход 20-ти значное число (номер счета)
     и возвращает маску номера по правилу **XXXX
     """
-    account_number_str = str(account_number)
+    # Регистрация входных данных
+    logger.info(f"Входные данные: card_number={get_mask_account}")
+
+    # Проверка типа аргумента
     if type(account_number) is not int:
+        logger.warning("Тип аргумента не является целым числом.")
         return "Введите 20-ти значное число"
-    elif len(account_number_str) != 20:
+
+    # Преобразование аргумента в строку
+    account_number_str = str(account_number)
+
+    # Проверка длины строки
+    if len(account_number_str) != 20:
+        logger.warning("Длина строки не равна 16 символам.")
         return "Введите 20-ти значное число"
-    else:
-        return f"**{account_number_str[-4:]}"
+
+    # Регистрация результата
+    logger.info(f"Маскированный номер карты: {get_mask_account}")
+
+    # Форматирование номера карты
+    return f"**{account_number_str[-4:]}"
+
+
+if __name__ == '__main__':
+    # Правильное значение
+    print(get_mask_account(12345678901234561234))  # Должна вернуть замаскированное значение
+
+    # Неправильные значения
+    print(get_mask_account(123))  # Длина меньше 16 символов
+    print(get_mask_account(12.34))  # Тип не int
+    print(get_mask_account(123456789012345621234)) # Длина Bolshe 16 символове
 
 
 #####################################################################
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 ###########################################################################################
 import json
 import os
+from src.logging_utils import logger
 
 
 def converting_data_into_a_dict_list(data_file: int | str | bytes) -> list | None:
@@ -9,21 +10,29 @@ def converting_data_into_a_dict_list(data_file: int | str | bytes) -> list | Non
     и возвращает список словарей с данными о финансовых транзакциях.
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
     """
+
+    # Регистрация входных данных
+    logger.info(f"Входные данные: data_file={data_file}")
+
     if data_file:
         try:
+            # Проверка существует ли указанный путь
             if os.path.exists(data_file):
                 with open(data_file, encoding="utf-8") as json_file:
                     operations_data = json.load(json_file)
                 if isinstance(operations_data, list):
+                    logger.info(f"Исходящие данные: operations_data={operations_data}")
                     return operations_data
                 else:
+                    logger.error(f"Тип аргумента не является списком.")
                     return []
             else:
-                print("Путь не правильный")
+                logger.warning("Путь не правильный")
                 return []
         except (json.JSONDecodeError, FileNotFoundError) as e:
-            print(f"Возникла вот такая ошибка: {e}")
+            logger.error(f"Возникла ошибка: {e}")
             return []
+    logger.error("Обрабатывать нечего")
     return []
 
 
