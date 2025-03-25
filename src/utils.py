5bb8412ffe5540bab3f08d3a5950f18d98ba0f50
Aleksey Zvyagintsev <alex0236889@gmail.com>
2025-03-25 22:55:17 +0300

    В модуле utils.py дописана функция получения из excel-файла DataFrame

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,10 @@
 ###########################################################################################
+import csv
 import json
 import os
-import csv
+
+import pandas as pd
+from pandas import DataFrame
 
 from src.logging_utils import logger
 
@@ -41,12 +44,16 @@ def converting_data_from_json_to_a_dict_list(data_file: int | str | bytes) -> li
 
 # if __name__ == "__main__":
 #     file_with_operations = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "operations.json")
-#     operations_list = converting_data_into_a_dict_list(file_with_operations)
-#     print(operations_list)
+#     operations_list = converting_data_from_json_to_a_dict_list(file_with_operations)
+#     print(operations_list[0])
 
 
 def converting_data_from_csv_to_dict_list(data_file: int | str | bytes) -> list | None:
-
+    """
+    Функцию принимает на вход путь до csv-файла
+    и возвращает список словарей с данными о финансовых транзакциях.
+    Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
+    """
     # Регистрация входных данных
     logger.info(f"Предложен путь: data_file={data_file}")
 
@@ -56,7 +63,7 @@ def converting_data_from_csv_to_dict_list(data_file: int | str | bytes) -> list 
             logger.info("Пытаемся открыть файл и получить данные.")
             if os.path.exists(data_file):
                 with open(data_file, encoding="utf-8") as csv_file:
-                    operations_data = list(csv.DictReader(csv_file, delimiter=';'))
+                    operations_data = list(csv.DictReader(csv_file, delimiter=";"))
                 if isinstance(operations_data, list):
                     logger.info("Полученные данные являются списком.")
                     return operations_data
@@ -73,13 +80,38 @@ def converting_data_from_csv_to_dict_list(data_file: int | str | bytes) -> list 
     return []
 
 
-if __name__ == "__main__":
-    file_with_operations = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data/transactions.csv')
-    operations_list = converting_data_from_csv_to_dict_list(file_with_operations)
-    print(operations_list[0: 2])
+# if __name__ == "__main__":
+#     file_with_operations = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data/transactions.csv")
+#     operations_list = converting_data_from_csv_to_dict_list(file_with_operations)
+#     print(operations_list[0])
 
-def converting_data_from_xlsx_to_dict_list():
-    pass
 
+def converting_data_from_xlsx_to_dataframe(data_file: int | str | bytes) -> DataFrame | None:
+    """
+    Функцию принимает на вход путь до excel-файла
+    и возвращает DataFrame с финансовыми транзакциями.
+    Если файл пустой или путь не верный, функция ничего не возвращает.
+    """
+    # Регистрация входных данных
+    logger.info(f"Предложен путь: data_file={data_file}")
+    if data_file:
+        try:
+            # Проверка существует ли указанный путь
+            logger.info("Пытаемся открыть файл и получить данные.")
+            if os.path.exists(data_file):
+                operations_data = pd.read_excel(data_file)
+                return operations_data
+            else:
+                logger.warning("Путь не правильный.")
+        except Exception as e:
+            logger.error(f"Возникла ошибка: {e}")
+    logger.error("Обрабатывать нечего.")
+
+
+# if __name__ == "__main__":
+#     file_with_operations = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data/transactions_excel.xlsx")
+#     operations_list = converting_data_from_xlsx_to_dataframe(file_with_operations)
+#     print(operations_list.shape)
+#     print(operations_list.head())
 
 ############################################################################################
